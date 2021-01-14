dbc48adfc475ed799c329882fd7eae3b7ab94089
npsedhain <npsedhain@gmail.com>
2021-01-14 12:01:59 +0545

    Write tests. WIP.

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders cafe react header', () => {
+  render(
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+  );
+  const linkElement = screen.getByText(/Cafe React/i);
+  expect(linkElement).toBeInTheDocument();
+});
